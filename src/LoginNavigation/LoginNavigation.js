4a1fe3297894484c39d4e7569f9fc2ccab8b3227
Jason Lucas <jlucas5280@gmail.com>
2017-10-28 14:06:35 -0600

    audio

--- src/LoginNavigation/LoginNavigation.js
+++ src/LoginNavigation/LoginNavigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import introMusic from '../assets/05-Electric-Worm.m4a';
 
 const LoginNavigation = ( { skipLogin, emailLogin } ) => {
 
@@ -11,10 +12,13 @@ const LoginNavigation = ( { skipLogin, emailLogin } ) => {
             <button className='login-btn login-btn3' onClick={ emailLogin }>Login with Email</button>
             <button className='login-btn login-btn4'>Create an Account</button>
             <button className='login-btn login-btn4' onClick={ skipLogin }>Skip to Shindig</button>
-
           </div>
         </div>
-                    <p className='copyright'>Copyright © 2017 1705 Development, Inc.</p>
+        <p className='copyright'>Copyright © 2017 1705 Development, Inc.</p>
+        <audio poster={introMusic}autoPlay>
+          <source src={introMusic} ></source>
+          <source src={introMusic} ></source>
+        </audio>
       </div>
     );
   }
