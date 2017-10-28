2af391f067dc7772ba9f620f386f7f6bc44312b1
Jason Lucas <jlucas5280@gmail.com>
2017-10-28 13:20:50 -0600

    component structure

--- src/LoginNavigation/LoginNavigation.js
+++ src/LoginNavigation/LoginNavigation.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import Cube from '../cube/cube.js';
 
-
-const Home = () => {
+const LoginNavigation = ( { skipLogin, emailLogin } ) => {
 
     return (
-      <div className='home-outer-container'>
-        < Cube />
+      <div className='backdrop'>
         <div className='outer-button-container'>
           <div className='inner-button-container'>
             <button className='login-btn login-btn1'>Login with Facebook</button>
             <button className='login-btn login-btn2'>Login with Google</button>
-            <button className='login-btn login-btn3'>Login with Email</button>
+            <button className='login-btn login-btn3' onClick={ emailLogin }>Login with Email</button>
             <button className='login-btn login-btn4'>Create an Account</button>
+            <button className='login-btn login-btn4' onClick={ skipLogin }>Skip to Shindig</button>
+
           </div>
         </div>
+                    <p className='copyright'>Copyright © 2017 1705 Development, Inc.</p>
       </div>
     );
   }
 
-export default Home;
+export default LoginNavigation;
