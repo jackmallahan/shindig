a668cd1c47f71fd890b5897dab59a4f7786b3a08
Jack Mallahan <jackmallahan@gmail.com>
2017-10-31 14:23:59 -0600

    Add yml file and update knexfile for CI

--- knexfile.js
+++ knexfile.js
@@ -12,6 +12,18 @@ module.exports = {
     useNullAsDefault: true
   },
 
+  test: {
+  client: 'pg',
+  connection: process.env.DATABASE_URL || 'postgres://localhost/jetfueltest',
+  useNullAsDefault: true,
+  migrations: {
+    directory: './db/migrations'
+  },
+  seeds: {
+    directory: './db/test/seeds'
+    }
+  },
+
   staging: {
     client: 'postgresql',
     connection: {
