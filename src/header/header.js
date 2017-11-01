ea76a6d31abcbee66a88931cf856512234f8fd38
Jason Lucas <jlucas5280@gmail.com>
2017-11-01 09:33:04 -0600

    menu-icon

--- src/header/header.js
+++ src/header/header.js
@@ -6,6 +6,11 @@ const Header = ( { displayNavigation }) => {
     <div className='header-outer-container'>
       <div className='header-inner-container' onClick={ displayNavigation }>
         <h1 className='header-title flicker3'>S<span className='flicker2'>hi</span>nd<span className='flicker1'>i</span>g</h1>
+        <div className='menu-icon'>
+          <div className='menu-bar'></div>
+          <div className='menu-bar'></div>
+          <div className='menu-bar'></div>
+        </div>
       </div>
     </div>
   );
