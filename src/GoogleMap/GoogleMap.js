dea678ac8c6240a29f710f30380e6f3680a315cd
Jack Mallahan <jackmallahan@gmail.com>
2017-11-04 14:05:01 -0600

    Add unstyled map with marker at Turing

---
+++ src/GoogleMap/GoogleMap.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react"
+import { compose, withProps } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) =>
+  <GoogleMap
+    defaultZoom={14}
+    defaultCenter={{ lat: 39.7508, lng: -104.9966 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: 39.7508, lng: -104.9966 }} onClick={props.onMarkerClick} />}
+  </GoogleMap>
+)
+
+class Map extends Component {
+  state = {
+    isMarkerShown: true,
+  }
+
+  componentDidMount() {
+    this.delayedShowMarker()
+  }
+
+  delayedShowMarker = () => {
+    setTimeout(() => {
+      this.setState({ isMarkerShown: true })
+    }, 3000)
+  }
+
+  handleMarkerClick = () => {
+    this.setState({ isMarkerShown: false })
+    this.delayedShowMarker()
+  }
+
+  render() {
+    return (
+      <MyMapComponent
+        currentLat={this.props.currentLat}
+        currentLong={this.props.currentLong}
+        isMarkerShown={this.state.isMarkerShown}
+        onMarkerClick={this.handleMarkerClick}
+      />
+    )
+  }
+}
+
+export default Map;
