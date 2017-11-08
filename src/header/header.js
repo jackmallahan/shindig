f0af4b01f15c480535f69dc637421860d31403f8
Jack Mallahan <jackmallahan@gmail.com>
2017-11-08 10:41:14 -0700

    Lint header

--- src/header/header.js
+++ src/header/header.js
@@ -1,20 +1,19 @@
 import React from 'react';
+/* eslint disable no-warning, jsx-a11y/no-static-element-interactions*/
 
-const Header = ( { displayNavigation }) => {
-
-  return (
-    <div className='header-outer-container'>
-      <div className='header-inner-container' onClick={ displayNavigation }>
-        <h1 className='header-title flicker3'>S<span className='flicker2'>hi</span>nd<span className='flicker1'>i</span>g</h1>
-        <div className='menu-icon'>
-          <div className='menu-bar'></div>
-          <div className='menu-bar'></div>
-          <div className='menu-bar'></div>
-        </div>
+const Header = ({ displayNavigation }) => (
+  <div className="header-outer-container">
+    <div className="header-inner-container" onClick={displayNavigation}>
+      <h1 className="header-title flicker3">
+          S<span className="flicker2">hi</span>nd<span className="flicker1">i</span>g
+      </h1>
+      <div className="menu-icon">
+        <div className="menu-bar">
+        <div className="menu-bar">
+        <div className="menu-bar">
       </div>
     </div>
+  </div>
   );
-
-}
 
 export default Header;
