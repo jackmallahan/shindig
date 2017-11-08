fac39a51bf5d8b7320d4ef268051e2083d8e7211
Jason Lucas <jlucas5280@gmail.com>
2017-11-08 11:30:06 -0700

    cube linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "extends": ["airbnb", "prettier"],
+  "extends": ["airbnb"],
   "globals": {
     "fetch": "false",
     "localStorage": "false",
--- src/cube/cube.js
+++ src/cube/cube.js
@@ -3,33 +3,37 @@ import React from 'react';
 
 const Cube = () => {
 
-    return (
-      <div className='outer-cube-container'>
-        <div className='inner-cube-container'>
-          <div className='pole'></div>
-          <div className="cube-wrap">
-          	<div className="cube">
-          		<div className="cube-face front">
-                <div className='cube-background border-flicker1'></div>
-                <h1 className='flicker1'>S<span className='flicker2'>hi</span>nd<span className='flicker2'>i</span>g</h1>
-              </div>
-          		<div className="cube-face back">
-                <div className='cube-background border-flicker1'></div>
-                <h1 className='flicker3'>S<span className='flicker2'>hi</span>nd<span className='flicker1'>i</span>g</h1>
-              </div>
-          		<div className="cube-face left">
-                <div className='cube-background border-flicker2'></div>
-                <h1 className='flicker2'>S<span className='flicker1'>hi</span>nd<span className='flicker1'>i</span>g</h1>
-              </div>
-          		<div className="cube-face right">
-                <div className='cube-background border-flicker2'></div>
-                <h1 className='flicker1'>S<span className='flicker2'>hi</span>nd<span className='flicker2'>i</span>g</h1>
-              </div>
-          	</div>
-          </div>
+  return (
+    <div className="outer-cube-container">
+      <div className="inner-cube-container">
+        <div className="pole"></div>
+        <div className="cube-wrap">
+          <div className="cube">
+            <div className="cube-face front">
+              <div className="cube-background border-flicker1"></div>
+              <h1 className="flicker1">S<span className="flicker2">hi
+                </span>nd<span className="flicker2">i</span>g</h1>
+            </div>
+        		<div className="cube-face back">
+              <div className="cube-background border-flicker1"></div>
+              <h1 className="flicker3">S<span className="flicker2">hi
+                </span>nd<span className="flicker1">i</span>g</h1>
+            </div>
+        		<div className="cube-face left">
+              <div className="cube-background border-flicker2"></div>
+              <h1 className="flicker2">S<span className="flicker1">hi
+                </span>nd<span className="flicker1">i</span>g</h1>
+            </div>
+        		<div className="cube-face right">
+              <div className="cube-background border-flicker2"></div>
+              <h1 className="flicker1">S<span className="flicker2">hi
+                </span>nd<span className="flicker2">i</span>g</h1>
+            </div>
+        	</div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default Cube;
